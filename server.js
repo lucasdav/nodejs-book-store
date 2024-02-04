837f2e3229094ab78c7f05f08d39308f94323365
lucasdav <lucassouzadavanso@gmail.com>
2024-02-04 19:26:36 -0300

    conexao com mongodb

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
-import http from "http";
+// import http from "http";
+import "dotenv/config";
+import app from "./src/app.js";
 
 const PORT = 3000;
 
-const rotas = {
-    "/": "Curso de Node.js",
-    "/livros": "Entrei na rota livros",
-    "/autores": "Entrei na rota autores"
-};
-
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {"Content-Type": "text/plain"});
-    res.end(rotas[req.url])
-});
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log("servidor escutando");
 });
+
+// abaixo exemplo com htttp
+// const rotas = {
+//     "/": "Curso de Node.js",
+//     "/livros": "Entrei na rota livros",
+//     "/autores": "Entrei na rota autores"
+// };
+
+// abaixo exemplo com http
+// const server = http.createServer((req, res) => {
+//     res.writeHead(200, {"Content-Type": "text/plain"});
+//     res.end(rotas[req.url])
+// });
+
+// abaixo exemplo com http
+// server.listen(PORT, () => {
+//     console.log("servidor escutando");
+// });
