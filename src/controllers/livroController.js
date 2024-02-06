d8f8af2ad831b37809a2ecb254f2ac9a55d065f0
lucasdav <lucassouzadavanso@gmail.com>
2024-02-05 23:02:18 -0300

    embeding livro com autor

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -1,3 +1,4 @@
+import { autor } from "../models/Autor.js";
 import livro from "../models/Livro.js";
 
 class LivroController {
@@ -22,8 +23,11 @@ class LivroController {
   };
 
   static async cadastrarLivro (req, res) {
+    const novoLivro = req.body;
     try {
-      const novoLivro = await livro.create(req.body);
+      const autorEncontrado = await autor.findById(novoLivro.autor);
+      const livroCompleto = { ...novoLivro, autor: { ...autorEncontrado._doc }};
+      const livroCriado = await livro.create(livroCompleto);
       res.status(201).json({ message: "criado com sucesso", livro: novoLivro });
     } catch (erro) {
       res.status(500).json({ message: `${erro.message} - falha ao cadastrar livro` });
@@ -47,6 +51,16 @@ class LivroController {
       res.status(200).json({ message: "livro excluído com sucesso" });
     } catch (erro) {
       res.status(500).json({ message: `${erro.message} - falha na exclusão` });
+    }
+  };
+
+  static async listarLivrosPorEditora (req, res) {
+    const editora = req.query.editora;
+    try {
+      const livrosPorEditora = await livro.find({ editora: editora });
+      res.status(200).json(livrosPorEditora)
+    } catch (erro) {
+      res.status(500).json({ message: `${erro.message} - falha na busca` });
     }
   };
 };
--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -3,7 +3,9 @@ import LivroController from "../controllers/livroController.js";
 
 const routes = express.Router();
 
+// ordenar as rotas a baixa da maior complexidade para menor
 routes.get("/livros", LivroController.listarLivros);
+routes.get("/livros/busca", LivroController.listarLivrosPorEditora);
 routes.get("/livros/:id", LivroController.listarLivroPorId);
 routes.post("/livros", LivroController.cadastrarLivro);
 routes.put("/livros/:id", LivroController.atualizarLivro);
